2ed49835894a58d9b4d35a6860ed78dd11a7d5c2
Nicco Kunzmann <niccokunzmann@rambler.ru>
2020-03-01 09:56:54 +0100

    map languages where necessary

--- metadata/link.sh
+++ metadata/link.sh
@@ -5,9 +5,23 @@
 
 set -e
 
+transifex=../documentation/transifex-github-integration.yml
 end="all ok, no errors"
 
-function link() {
+function linkToLanguage() {
+  local lang=$1
+  local country=$2
+  mkdir -p $lang
+  if [ -d $lang-$country ] && ! [ -L $lang-$country ]; then
+    echo "ERROR: clean up $lang-$country"
+    end=""
+    return
+  fi
+  rm -f $lang-$country $lang-r$country || true
+  ln -sT $lang $lang-$country 
+}
+
+function linkToCountry() {
   local lang=$1
   local country=$2
   mkdir -p $lang-r$country
@@ -22,19 +36,26 @@ function link() {
     return
   fi
   rm -f $lang $lang-$country || true
-  ln -sT $lang-r$country $lang-$country 
+  ln -sT $lang-r$country $lang-$country
   ln -sT $lang-r$country $lang
+  if ! cat $transifex | grep -q "$lang-$country: $lang-r$country"; then
+    echo "ERROR: please update the transifex settings in $transifex to map $lang-$country: $lang-r$country"
+    end=""
+  fi
 }
 
-link de DE
-link pt BR
-link fr FR
-link pl PL
-link jp JP
-link es ES
-link ru RU
+
+linkToLanguage de DE
+linkToCountry pt BR
+linkToLanguage fr FR
+linkToLanguage pl PL
+linkToLanguage jp JP
+linkToLanguage es ES
+linkToLanguage ru RU
 
 if [ -n "$end" ]; then
-    echo $end
+  echo $end
+  ls -l
+  echo $end
 fi
 
